214a8c6f90dc712f33ad49b79276c6035f9ebc5a
Stutie <stutiebanerjie@gmail.com>
2026-03-03 19:35:41 -0500

    Create TicketPlan.java

---
+++ exam1/practice3/TicketPlan.java
@@ -0,0 +1,17 @@
+/* 
+   Name: Stutie Banerjie
+   Date: 3.3.2026
+   Asg: Practice Exam 3 - Class File
+   Desc: Help resort track ticket plan purchases
+         and calculate ticket plan prices
+*/
+
+public class TicketPlan {
+   
+   // instance fields
+   
+   // no-arg constructor
+   
+   // constructor with arguments
+   
+}
